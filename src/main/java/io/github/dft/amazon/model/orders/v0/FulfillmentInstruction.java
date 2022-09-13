4c958b5ca060c71af07c083069c0a740f2c6f9e0
Dhruv.Fefar <dhruv.fefar@dakshyafreondtech.com>
2022-09-13 21:01:05 +0530

    orders pojo implements in amazon-sp-api

--- src/main/java/io/github/dft/amazon/model/orders/v0/FulfillmentInstruction.java
+++ src/main/java/io/github/dft/amazon/model/orders/v0/FulfillmentInstruction.java
@@ -1,11 +1,13 @@
 package io.github.dft.amazon.model.orders.v0;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FulfillmentInstruction {
 
+    @JsonProperty("FulfillmentSupplySourceId")
     private String fulfillmentSupplySourceId;
 }
