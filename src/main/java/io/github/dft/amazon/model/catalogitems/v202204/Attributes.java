771fc75ce011b92a139c2f08b0652148cfeb803b
milan.lathiya <milan.lathiya@dakshyafreondtech.com>
2022-09-22 16:36:06 +0530

    Added catalogItems methods.

---
+++ src/main/java/io/github/dft/amazon/model/catalogitems/v202204/Attributes.java
@@ -0,0 +1,17 @@
+package io.github.dft.amazon.model.catalogitems.v202204;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Attributes {
+
+    private List<UnitCount> unitCount;
+    private List<ItemName> itemName;
+}
