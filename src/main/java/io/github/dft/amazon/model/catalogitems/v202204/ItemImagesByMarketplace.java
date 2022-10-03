37cec22d5365fbc01cb208d0afb707a5a9a2f88d
Dhruv.Fefar <dhruv.fefar@dakshyafreondtech.com>
2022-10-03 18:20:23 +0530

    catelog payloag created in sp-api amazon

---
+++ src/main/java/io/github/dft/amazon/model/catalogitems/v202204/ItemImagesByMarketplace.java
@@ -0,0 +1,13 @@
+package io.github.dft.amazon.model.catalogitems.v202204;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ItemImagesByMarketplace {
+
+    private String marketplaceId;
+    private List<ItemImage> images;
+}
