939292818f3f2f88d849122d5299649e21dbe5d5
Dhruv.Fefar <dhruv.fefar@dakshyafreondtech.com>
2023-06-21 11:18:30 +0530

    RA-1 added fee filed Other transaction class

--- src/main/java/io/github/dft/amazon/model/settelmentreport/SettlementReport.java
+++ src/main/java/io/github/dft/amazon/model/settelmentreport/SettlementReport.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
