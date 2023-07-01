72dc687ada2b478af975b0adfef85dabc269f1c4
Rakesh Dobariya <r_dobariya50@yahoo.com>
2023-07-01 17:36:15 +0530

    Updated order API with PII.

--- src/main/java/io/github/dft/amazon/AmazonSPOrders.java
+++ src/main/java/io/github/dft/amazon/AmazonSPOrders.java
@@ -1,11 +1,13 @@
 package io.github.dft.amazon;
 
 import com.amazonaws.http.HttpMethodName;
-import com.google.common.util.concurrent.RateLimiter;
 import io.github.dft.amazon.constantcode.ConstantCodes;
 import io.github.dft.amazon.model.AmazonCredentials;
 import io.github.dft.amazon.model.handler.JsonBodyHandler;
 import io.github.dft.amazon.model.orders.v0.*;
+import io.github.dft.amazon.model.tokens.v202103.CreateRestrictedDataTokenRequest;
+import io.github.dft.amazon.model.tokens.v202103.CreateRestrictedDataTokenResponse;
+import io.github.dft.amazon.model.tokens.v202103.RestrictedResource;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.utils.URIBuilder;
@@ -13,23 +15,34 @@ import org.apache.http.client.utils.URIBuilder;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class AmazonSPOrders extends AmazonSellingPartnerSdk {
 
-    private final RateLimiter rateLimiter;
+    private static final String AMAZON_ORDERS_PII_PATH = "/orders/v0/orders";
+    private static final List<String> AMAZON_ORDERS_PII_DATA_ELEMENTS = Arrays.asList("buyerInfo", "shippingAddress");
+
+    private String restrictedAccessToken;
+    private ZonedDateTime restrictedAccessTokenExpireIn;
+    private final AmazonSPTokens amazonSPTokens;
 
     @SneakyThrows
     public AmazonSPOrders(AmazonCredentials amazonCredentials) {
         super(amazonCredentials);
-        this.rateLimiter = RateLimiter.create(0.8);
+        this.amazonSPTokens = new AmazonSPTokens(amazonCredentials);
     }
 
     @SneakyThrows
-    public GetOrdersResponse getOrders(HashMap<String, String> params) {
+    public GetOrdersResponse getOrders(HashMap<String, String> params, boolean isPII) {
 
         final var signRequest = signRequest(ConstantCodes.ORDERS_API_V0, HttpMethodName.GET, params, null);
 
+        String token = isPII ? getPIIAccessToken(createRestrictedDataToken(AMAZON_ORDERS_PII_PATH)) : amazonCredentials.getAccessToken();
         URIBuilder uriBuilder = new URIBuilder(sellingRegionEndpoint + ConstantCodes.ORDERS_API_V0);
         addParameters(uriBuilder, params);
 
@@ -37,7 +50,7 @@ public class AmazonSPOrders extends AmazonSellingPartnerSdk {
         HttpRequest request = HttpRequest.newBuilder(uri)
                 .header(ConstantCodes.HTTP_HEADER_ACCEPTS, ConstantCodes.HTTP_HEADER_VALUE_APPLICATION_JSON)
                 .header(ConstantCodes.HTTP_HEADER_CONTENT_TYPE, ConstantCodes.HTTP_HEADER_VALUE_APPLICATION_JSON)
-                .header(ConstantCodes.HTTP_HEADER_X_AMZ_ACCESS_TOKEN, amazonCredentials.getAccessToken())
+                .header(ConstantCodes.HTTP_HEADER_X_AMZ_ACCESS_TOKEN, token)
                 .header(ConstantCodes.HTTP_HEADER_AUTHORIZATION, signRequest.getHeaders().get(ConstantCodes.HTTP_HEADER_AUTHORIZATION))
                 .header(ConstantCodes.HTTP_HEADER_X_AMZ_SECURITY_TOKEN, signRequest.getHeaders().get(ConstantCodes.HTTP_HEADER_X_AMZ_SECURITY_TOKEN))
                 .header(ConstantCodes.X_AMZ_DATE, signRequest.getHeaders().get(ConstantCodes.X_AMZ_DATE))
@@ -47,6 +60,31 @@ public class AmazonSPOrders extends AmazonSellingPartnerSdk {
         HttpResponse.BodyHandler<GetOrdersResponse> handler = new JsonBodyHandler<>(GetOrdersResponse.class);
 
         return getRequestWrapped(request, handler);
+    }
+
+    public CreateRestrictedDataTokenRequest createRestrictedDataToken(String restrictedTokenPath) {
+        CreateRestrictedDataTokenRequest createRestrictedDataTokenRequest = new CreateRestrictedDataTokenRequest();
+        List<RestrictedResource> restrictedResources = new ArrayList<>();
+
+        RestrictedResource restrictedResource = new RestrictedResource();
+        restrictedResource.setMethod("GET");
+        restrictedResource.setPath(restrictedTokenPath);
+        restrictedResource.setDataElements(AMAZON_ORDERS_PII_DATA_ELEMENTS);
+
+        restrictedResources.add(restrictedResource);
+        createRestrictedDataTokenRequest.setRestrictedResources(restrictedResources);
+
+        return createRestrictedDataTokenRequest;
+    }
+
+    private String getPIIAccessToken(CreateRestrictedDataTokenRequest createRestrictedDataTokenRequest) {
+        if(restrictedAccessToken == null || ZonedDateTime.now(ZoneOffset.UTC).isAfter(this.restrictedAccessTokenExpireIn)) {
+            CreateRestrictedDataTokenResponse createRestrictedDataTokenResponse = amazonSPTokens.createRestrictedDataToken(createRestrictedDataTokenRequest);
+            this.restrictedAccessToken = createRestrictedDataTokenResponse.getRestrictedDataToken();
+            this.restrictedAccessTokenExpireIn = ZonedDateTime.now(ZoneOffset.UTC).plusSeconds(createRestrictedDataTokenResponse.getExpiresIn());
+        }
+
+        return restrictedAccessToken;
     }
 
     @SneakyThrows
